0fb45a18a52b895a895bf10608bf8c7bc57ba637
Bubbler-4 <bubbler9903@gmail.com>
2025-08-03 16:58:32 +0900

    solve 003, 004

--- submissions/task003.py
+++ submissions/task003.py
@@ -1,1 +1,3 @@
-def p(g): return g
+def p(g):
+	for i in range(1,7):
+		if(a:=g[:i]*9)[:6]==g:return[[x*2 for x in r]for r in a[:9]]
--- submissions/task004.py
+++ submissions/task004.py
@@ -1,1 +1,8 @@
-def p(g): return g
+def p(g):
+	for r,R in enumerate(g):
+		for c,C in enumerate(R):
+			if 0<C==g[r][c-1]==g[r-1][c]:
+				g[r-1][c-1]=C;g[r-1][c]=0;g[r][c]=0;c-=1
+				while g[r][c]:c-=1
+				g[r][c]=C
+	return[[0]+r[:-1]for r in g]
